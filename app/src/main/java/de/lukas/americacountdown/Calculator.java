e59c4decb44fb4ac4b332a54c226785443b60903
SHRipper <lukas.schaef@gmx.de>
2016-05-08 20:51:24 +0200

    initial commit

---
+++ app/src/main/java/de/lukas/americacountdown/Calculator.java
@@ -0,0 +1,29 @@
+package de.lukas.americacountdown;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by Lukas on 07.05.2016.
+ */
+public class Calculator {
+
+    public static String getLeftDays() {
+        SimpleDateFormat myFormat = new SimpleDateFormat("dd MM yyyy");
+        String endDate = "11 09 2016";
+        Calendar c = Calendar.getInstance();
+
+        long daysLeft = 0;
+        try {
+            Date date1 = c.getTime();
+            Date date2 = myFormat.parse(endDate);
+            daysLeft = date2.getTime() - date1.getTime();
+            daysLeft = TimeUnit.DAYS.convert(daysLeft, TimeUnit.MILLISECONDS);
+        } catch (java.text.ParseException e) {
+            e.printStackTrace();
+        }
+        return (daysLeft + "");
+    }
+}
---
+++ app/src/main/java/de/lukas/americacountdown/MainActivity.java
@@ -0,0 +1,78 @@
+package de.lukas.americacountdown;
+
+import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.widget.DatePicker;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    DatePicker datePicker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        datePicker = (DatePicker) findViewById(R.id.datePicker);
+        datePicker.setEnabled(false);
+
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+
+        TextView txtDaysLeft = (TextView) findViewById(R.id.txtDays);
+        txtDaysLeft.setText(Calculator.getLeftDays());
+
+        //der MyAlarmReceiver wurde nie aufgerufen!
+        //Intent i = new Intent(this, MyAlarmReceiver.class);
+        //PendingIntent pi = PendingIntent.getBroadcast(this,0,i,PendingIntent.FLAG_CANCEL_CURRENT);
+
+        InitAlarmManager.setAlarmManager(this);
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_countdown) {
+
+        } else if (id == R.id.nav_settings) {
+
+        } else if (id == R.id.nav_about) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+
+}
